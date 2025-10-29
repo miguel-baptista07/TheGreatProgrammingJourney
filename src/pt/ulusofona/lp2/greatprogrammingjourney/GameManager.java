1ea9a1cd1ca460ebfc2313446d46701b7471baea
Miguel <miguelkevlin23gmail.com>
2025-10-29 09:27:07 +0000

    Adicionar Classe GameManager

--- src/pt/ulusofona/lp2/greatprogrammingjourney/GameManager.java
+++ src/pt/ulusofona/lp2/greatprogrammingjourney/GameManager.java
@@ -1,4 +1,51 @@
 package pt.ulusofona.lp2.greatprogrammingjourney;
 
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class GameManager {
+    public boolean createInitialBoard(String[][] playerInfo, int worldSize) {
+        return false;
+    }
+
+    public String getImagePng(int nrSquare) {
+        return null;
+    }
+
+    public String[] getProgrammerInfo(int id) {
+        return null;
+    }
+
+    public String getProgrammerInfoAsStr(int id) {
+        return null;
+    }
+
+    public String[] getSlotInfo(int position) {
+        return null;
+    }
+
+    public int getCurrentPlayerID() {
+        return 0;
+    }
+
+    public boolean moveCurrentPlayer(int nrSpaces) {
+        return false;
+    }
+
+    public boolean gameIsOver() {
+        return false;
+    }
+
+    public ArrayList<String> getGameResults() {
+        return null;
+    }
+
+    public JPanel getAuthorsPanel() {
+        return null;
+    }
+
+    public HashMap<String, String> customizeBoard() {
+        return null;
+    }
 }
