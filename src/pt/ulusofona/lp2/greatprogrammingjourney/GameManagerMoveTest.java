7cc62a2a7c5f091d69c832421567d6a951ff680c
Miguel <miguelkevlin23@gmail.com>
2025-11-19 08:43:20 +0000

    Submissao 1 codigo correto

--- src/pt/ulusofona/lp2/greatprogrammingjourney/GameManagerMoveTest.java
+++ src/pt/ulusofona/lp2/greatprogrammingjourney/GameManagerMoveTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TestGameManagerMove {
+public class GameManagerMoveTest {
 
     private GameManager gm;
     private final int WORLD_SIZE = 20;
